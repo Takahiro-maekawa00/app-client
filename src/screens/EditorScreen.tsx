12cc9ee686f876e0345e5f3fe4eb544dd4df1670
Takahiro-maekawa00 <119389005+Takahiro-maekawa00@users.noreply.github.com>
2025-07-07 20:47:50 +0900

    Initialize React Native skeleton

---
+++ src/screens/EditorScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function EditorScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Editor Screen (WIP)</Text>
+    </View>
+  );
+}
